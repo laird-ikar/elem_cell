ba57f34b5ff1c589a80b2d3d1d1b2e44a496a191
laird-ikar <bguyot@student.42mulhouse.fr>
2021-12-21 13:01:04 +0100

    shortening somewhat the code

--- srcs/cell.c
+++ srcs/cell.c
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 20:49:31 by bguyot            #+#    #+#             */
-/*   Updated: 2021/12/21 01:57:33 by bguyot           ###   ########.fr       */
+/*   Updated: 2021/12/21 12:57:59 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,28 @@ int	main(int ac, char *av[])
 	int		*rules;
 	t_world	world;
 
-	rules = int_to_rules(ft_atoi(av[3]));
-	world = str_to_world(av[1], av[2]);
-	if (ac == 5 && world.size != -42)
-		tomorrow(av[1],
-			world,
-			rules,
-			ft_atoi(av[4]));
+	rules = NULL;
+	world.matter = NULL;
+	if (ac == 5)
+	{
+		rules = int_to_rules(ft_atoi(av[3]));
+		world = str_to_world(av[1], av[2]);
+		if (world.size != -42)
+			tomorrow(av[1],
+				world,
+				rules,
+				ft_atoi(av[4]));
+	}
 	else
 		ft_putstr("Please indicate the charset that represent \e[1;31mdead\e[m \
 then \e[1;32malive\e[m,\ninitial \e[1;34mworld\e[m with that atoms and \
 \e[1;35mrule\e[m and finaly the \e[1;90mdoomsday\e[m\n\t\
 Exemple : \n\n\t ./elem_cell \"\e[31m.\e[32m#\e[m\" \"\e[34m...#...\e[m\" \
 \e[35m110\e[m \e[0;90m42\e[m\n");
-	free(rules);
-	free(world.matter);
+	if (rules)
+		free(rules);
+	if (world.matter)
+		free(world.matter);
 	return (0);
 }
 
@@ -51,7 +58,14 @@ void	tomorrow(char atoms[2], t_world yesterday,
 	{
 		today.size = yesterday.size;
 		today.matter = malloc(sizeof(int) * today.size);
-		dawn(&today, &yesterday, &gods_memory, rule);
+		i = 0;
+		while (i < today.size)
+		{
+			gods_memory = yesterday.matter[i] * 2
+				+ (i != today.size - 1) * yesterday.matter[i + 1]
+				+ (i != 0) * yesterday.matter[i - 1] * 4;
+			today.matter[i++] = rule[gods_memory];
+		}
 		tomorrow(atoms, today, rule, doomsday - 1);
 	}
 	if (today.matter)
@@ -100,27 +114,4 @@ int	*int_to_rules(int encoded_rules)
 		i--;
 	}
 	return (rules);
-}
-
-void	dawn(t_world *today, t_world *yesterday, int *gods_memory, int *rule)
-{
-	int	i;
-
-	i = 0;
-	while (i < today->size)
-	{
-		if (i == 0 && i == today->size - 1)
-			*gods_memory = rule[yesterday->matter[i] * 2];
-		else if (i == 0)
-			*gods_memory = rule[yesterday->matter[i] * 2
-				+ yesterday->matter[i + 1]];
-		else if (i == today->size - 1)
-			*gods_memory = rule[yesterday->matter[i - 1] * 4
-				+ yesterday->matter[i] * 2];
-		else
-			*gods_memory = rule[yesterday->matter[i - 1] * 4
-				+ yesterday->matter[i] * 2
-				+ yesterday->matter[i + 1]];
-		today->matter[i++] = *gods_memory;
-	}
 }
